5494560d0618e21841faaf9c3060e5d241bb9cd4
dasilva08 <dasilva08>
2017-02-06 15:15:59 -0200

    descrição na home do projeto!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Bem vindos ao Workflow Dexter';
 
 }
